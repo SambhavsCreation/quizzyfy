14612ba65f603f6a8de78ba64ae44db53b01a92c
Sambhav Sharma <visualstudiosam7@gmail.com>
2022-05-18 15:10:13 +0530

    First commit. Added the basic layout of the screen.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App()
+{
+    return (
+        <div>
+            <img src={require("./assets/blob 5.png")} alt="null" className="blob1"/>
+            <img src={require("./assets/blob 6.png")} alt="null" className="blob2"/>
+        </div>
+    )
 }
-
-export default App;
